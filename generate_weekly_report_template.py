a55c776358481d3bc01c87f78b2a35c29ccfd23e
Ho Sung Lee <lee.ho.sung@gmail.com>
2017-06-24 20:49:25 +0900

    health data 로 부터 weekly report 생성할 수 있도록 준비

--- generate_weekly_report_template.py
+++ generate_weekly_report_template.py
@@ -1,16 +1,86 @@
-import datetime
+import csv
+from datetime import date
+from datetime import datetime
+from datetime import timedelta
 
 TEMPLATE_DIR = 'templates'
 OUTPUT_DIR = '/Users/tyson/git/leehosung.github.io/_drafts'
-contents = file('{}/{}'.format(TEMPLATE_DIR, 'weekly.md')).read()
+HEALTH_SOURCE_FILE = '/Users/tyson/Downloads/Health Data.csv'
+START_DATE = date(2017, 6, 21)
+WATER_GOAL = 2
+STEPS_GOAL = 10000
+CALORIES_GOAL = 1200
 
-def get_next_sunday():
-    today = datetime.date.today()
-    sunday = today + datetime.timedelta((6 - today.weekday()) % 7)
-    return sunday
+today = date.today()
+this_week_sunday = today + timedelta((6 - today.weekday()) % 7)
+this_week_sunday = this_week_sunday
+this_week_monday = this_week_sunday + timedelta(-6)
+next_week_monday = this_week_sunday + timedelta(1)
+next_week_sunday = this_week_sunday + timedelta(7)
 
-sunday = get_next_sunday()
+def get_health_data():
+    with open(HEALTH_SOURCE_FILE) as csvfile:
+        datareader = csv.DictReader(csvfile)
+        return filter(lambda x:
+                      datetime.strptime(x['Start'], '%d-%b-%Y %H:%M').date() >= this_week_monday and
+                      datetime.strptime(x['Start'], '%d-%b-%Y %H:%M').date() >= START_DATE and
+                      datetime.strptime(x['Finish'], '%d-%b-%Y %H:%M').date() < next_week_monday,
+                      datareader)
 
-file_name = sunday.strftime('%Y-%m-%d-%Y-W%W.md')
+def get_health_stat():
+    data = get_health_data()
+    water_success_days = 0
+    total_water = 0
+    step_success_days = 0
+    total_steps = 0
+    calory_success_days = 0
+    total_calories = 0
+
+    count = 0
+    for row in data:
+        water = float(row['Dietary Water (L)'])
+        if water >= WATER_GOAL:
+            water_success_days += 1
+        total_water += water
+
+        steps = float(row['Steps (count)'])
+        if steps >= STEPS_GOAL:
+            step_success_days += 1
+        total_steps += steps
+
+        # calory -> killo calory
+        calory = float(row['Dietary Calories (cal)']) / 1000
+        if calory <= CALORIES_GOAL:
+            calory_success_days += 1
+        total_calories += calory
+        count += 1
+
+    return dict(
+        average_water="%.2f" % (total_water/count),
+        water_success_days=water_success_days,
+        average_steps=int(total_steps/count),
+        step_success_days=step_success_days,
+        average_calories=int(total_calories/count),
+        calory_success_days=calory_success_days,
+        count=count
+    )
+
+def get_report_from_template():
+    contents = file('{}/{}'.format(TEMPLATE_DIR, 'weekly.md')).read()
+    values = dict(
+        this_week_year=this_week_monday.year,
+        this_week_number=this_week_monday.strftime('%W'),
+        this_week_monday=this_week_monday,
+        this_week_sunday=this_week_sunday,
+        next_week_year=next_week_monday.year,
+        next_week_number=next_week_monday.strftime('%W'),
+        next_week_monday=next_week_monday,
+        next_week_sunday=next_week_sunday,
+    )
+    values.update(get_health_stat())
+    return contents.format(**values)
+
+file_name = this_week_sunday.strftime('%Y-%m-%d-%Y-W%W.md')
 file_path = '{}/{}'.format(OUTPUT_DIR, file_name)
-open(file_path, 'w').write(contents.format(sunday.year, sunday.strftime('%W'), sunday))
+weekly_report = get_report_from_template()
+open(file_path, 'w').write(weekly_report)
--- templates/weekly.md
+++ templates/weekly.md
@@ -1,9 +1,53 @@
 ---
 layout: post
-title:  {}-W{}
-date:   {} 00:00:00
+title:  {this_week_year}-W{this_week_number}
+date:   {this_week_sunday} 00:00:00
 author: Lee Ho Sung
 categories: Weekly
 tags: weekly quantified_self
 ---
 
+## {this_week_year}년 {this_week_number}주차 결과({this_week_monday} ~ {this_week_sunday})
+
+### 학습
+
+| 구분                                        | 세션 수 |
+| ------------------------------------------- |:-------:|
+| 글쓰기                                      | 0       |
+| Pycon 준비하기                              | 0       |
+| 책 읽기                                     | 0       |
+
+### 건강 
+
+ * 물먹기: 평균 {average_water}L, 목표달성 {water_success_days}/{count}일
+ * 활동량: 평균 {average_steps}걸음, 목표달성 {step_success_days}/{count}일
+ * 식이: 평균 {average_calories}Kcal, 목표달성 {calory_success_days}/{count}일
+
+### 그외
+
+ * -
+
+### 평가 
+
+
+---
+
+## {next_week_year}년 {next_week_number}주차 목표({next_week_monday} ~ {next_week_sunday})
+
+### 학습
+
+| 구분                                        | 세션 수 |
+| ------------------------------------------- |:-------:|
+| 글쓰기                                      | 0       |
+| Pycon 준비하기                              | 0       |
+| 책 읽기                                     | 0       |
+
+### 건강
+
+* 하루에 물 2L 먹기 
+* 하루에 만보 이상 걷기
+* 하루에 1200Kcal 넘기지 않기 
+
+### 그외
+
+* 
